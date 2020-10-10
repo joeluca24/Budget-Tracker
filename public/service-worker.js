7c204331029a513c71218c5b73465679ab2d4ae8
joeluca24 <65137591+joeluca24@users.noreply.github.com>
2020-10-09 20:41:30 -0400

    initial app setup

---
+++ public/service-worker.js
@@ -0,0 +1,14 @@
+const FILES_TO_CACHE = [
+ '/',
+ '/index.html',
+ '/icons/icon-192x192.png',
+'/icons/icon-512x512.png',
+'/index.js',
+'/manifest.webmanifest',
+'/styles.css'
+];
+
+
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
+
